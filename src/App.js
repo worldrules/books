b754c69fc0333ca744da548d0237d4299f32f513
worldrulesdev <worldrulesdev@gmail.com>
2023-08-13 20:57:48 -0300

    kek

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import { useState } from "react";
+import BookCreate from './components/BookCreate'
 
 function App() {
     const [books, setBooks] = useState([]);
 
-    return <div>App</div>;
 
+    const createBook = (title) => {
+        console.log('need to add book with :', title)
+    };
+
+    return <div>
+        <BookCreate onCreate={createBook} />
+    </div>;
 }
 
 export default App;
--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,5 +1,23 @@
-function BookCreate() {
-    return <div>BookCreate</div>
+import { useState } from "react";
+
+function BookCreate({ onCreate }) {
+    const [title, setTitle] = useState('')
+
+    const handleChange = (e) => {
+        setTitle(e.target.value)
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onCreate(title);
+    };
+
+    return <div>
+        <form onSubmit={handleSubmit}>
+            <label>Title</label>
+            <input value={title} onChange={handleChange} />
+            <button>Create !</button>
+        </form>
+    </div>
 
 }
 
