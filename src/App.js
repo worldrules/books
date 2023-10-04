1f30b21122427fcf21b5820cfcc16b454af334fd
worldrulesdev <worldrulesdev@gmail.com>
2023-10-04 16:20:45 -0300

    kek

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import BookCreate from './components/BookCreate'
+import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
 import './index.css'
 
 
@@ -9,12 +10,16 @@ function App() {
 
     const createBook = (title) => {
         const updatedBooks = [
-            ...books, { id: 123, title }
+            ...books, {
+                id: Math.round(Math.random() * 9999),
+                title
+            },
         ];
         setBooks(updatedBooks);
     };
 
     return <div>
+        {books.length}
         <BookCreate onCreate={createBook} />
     </div>;
 }
