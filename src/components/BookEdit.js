16c581f47622526dcc5de4cae092536c2f67eb56
worldrulesdev <worldrulesdev@gmail.com>
2023-10-05 16:09:59 -0300

    Book edit start and add state in book show

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,5 +1,25 @@
-function BookEdit() {
-    return <div>BookEdit</div>
+import { useState } from 'react'
+
+function BookEdit({ book }) {
+    const [title, setTitle] = useState(book.title);
+
+    const handleChange = (e) => {
+        setTitle(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log("New title is", title)
+    }
+
+    return <form onSubmit={handleSubmit} className="book-edit">
+        <label>Title</label>
+        <input className="input" value={title} onChange={handleChange} />
+        <button className="button is-primary">
+            Save
+        </button>
+    </form>;
 
 }
 
