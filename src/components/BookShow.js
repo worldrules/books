c5605e971f8f547b742c62d6ec6d28cfbaaf7cd0
worldrulesdev <worldrulesdev@gmail.com>
2023-10-05 18:38:49 -0300

    app done

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -24,6 +24,10 @@ function BookShow({ book, onDelete, onEdit }) {
     }
 
     return <div className="book-show">
+        <img
+            src={`https://picsum.photos/seed/${book.id}/300/200`}
+            alt="kekw">
+        </img>
         <div>{content}</div>
         <div className="actions">
             <button className="edit" onClick={handleEditClick}>
